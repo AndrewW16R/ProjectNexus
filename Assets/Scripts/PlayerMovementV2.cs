f0d1121afc54fce793c762c6d761646e179e865b
AndrewRW <andrew12rw@gmail.com>
2024-02-13 14:52:58 -0600

    PlayerV2 movement testing + faster falling

---
+++ Assets/Scripts/PlayerMovementV2.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementV2 : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private BoxCollider2D coll;
+    private SpriteRenderer sprite;
+    private Animator anim;
+
+    private float initialGravity;
+    private bool fastFalling = false;
+    [SerializeField] private float fastFallGravMultiplier = 1.5f;
+
+    [SerializeField] private LayerMask jumpableGround;
+
+    //left/right input
+    private float dirX = 0;
+
+    [SerializeField] private float movementSpeed = 6f;
+    [SerializeField] private float jumpStrength = 14f;
+    private enum MovementState { idle, running, jumping, falling }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        initialGravity = rb.gravityScale;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Walking movement
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * movementSpeed, rb.velocity.y);
+
+    }
+
+    private void Update()
+    {
+
+        if(IsGrounded())
+        {
+            rb.gravityScale = initialGravity;
+            fastFalling = false;
+        }
+
+        //jumping
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpStrength);
+        }
+
+        //Updates sprite animation
+        UpdateAnimationUpdate();
+    }
+
+
+    private void UpdateAnimationUpdate()
+    {
+        //Local variable to hold the animation state
+        MovementState state;
+
+        if (dirX > 0f)
+        {
+            state = MovementState.running;
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            state = MovementState.running;
+            sprite.flipX = true;
+        }
+        else
+        {
+            state = MovementState.idle;
+        }
+
+        if (rb.velocity.y > .1f)
+        {
+            state = MovementState.jumping;
+        }
+        else if (rb.velocity.y < -.1f)
+        {
+            state = MovementState.falling;
+            if (fastFalling == false)
+            {
+                rb.gravityScale = rb.gravityScale * fastFallGravMultiplier;
+                fastFalling = true;
+            }
+        }
+
+        //Sets value for state parameter
+        anim.SetInteger("state", (int)state);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    }
+}
