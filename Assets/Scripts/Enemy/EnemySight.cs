81234d2bb0b8e75f64f13a50b9cad6000b34b6ef
AndrewRW <andrew12rw@gmail.com>
2024-04-23 18:04:07 -0500

    aStar AI pathing package imported

---
+++ Assets/Scripts/Enemy/EnemySight.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySight : MonoBehaviour
+{
+
+    public bool playerInSight;
+
+    public GameObject player;
+    
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+
+    }
+}
