5bd24df4e0420e7af84701278a741516f88a2161
AndrewRW <andrew12rw@gmail.com>
2024-04-23 20:44:01 -0500

    Very first pass of enemy pathfinding

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float speed = 100f;
+    public float nextWaypointDistance = 3f; //How close the enemy needs to be to the waypoint before moving on to the next one
+
+    Path path; //The current path which the enemy is following
+    int currentWaypoint = 0; //The current waypoint along the path which is being targetted
+    bool reachedEndOfPath = false; //Whether or not the end of the path has been reached
+
+    Seeker seeker;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, .5f); //Specified methed to invoke, amount of time to wait before calling method, repeat rate (currently set to every half-second
+       
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone()) //if a path is not currently being calculated
+        {
+            seeker.StartPath(rb.position, target.position, OnPathComplete); //Starts calculating path
+        }
+        
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if(!p.error) //if there are no errors calculating the path
+        {
+            path = p;
+            currentWaypoint = 0; //Start at beginning of newly calculated path
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (path == null) //end update if there is no path to follow
+        {
+            return;
+        }
+
+        if (currentWaypoint >= path.vectorPath.Count) //returns if enemy has reach the end of their path and there are no more waypoints
+        {
+            reachedEndOfPath = true;
+            return;
+        }
+        else
+        {
+            reachedEndOfPath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized; //Draws direction from position to the next waypoint
+        Vector2 force = direction * speed * Time.deltaTime;
+       // rb.AddForce(force);
+       rb.velocity = force;
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]); //sets distance float to distance from enemy to their next waypoint
+
+        if(distance < nextWaypointDistance)
+        {
+            currentWaypoint++;//Waypoint has been reached, time to move to next waypoint
+        }
+
+    }
+}
